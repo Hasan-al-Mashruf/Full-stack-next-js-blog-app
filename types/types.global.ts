684a6549dd9454496b1cb226bbedc217f4e2f8f1
Moeenuddin Ahmad <mashruf125@gmail.com>
2024-06-30 14:00:19 +0600

    typescript types is setup

---
+++ types/types.global.ts
@@ -0,0 +1,84 @@
+export interface IParams {
+  id: string;
+}
+export interface IUser {
+  id: string;
+  name: string;
+  email: string;
+  password?: string | null;
+  image?: string | null;
+  emailVerified?: Date | null;
+  blogs?: IBlog[];
+  follower?: IFollow[];
+  following?: IFollow[];
+  comment?: IComment[];
+  like?: ILike[];
+  reporter?: IReport[];
+  reported?: IReport[];
+  followingCat?: ICategory[];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface IFollow {
+  id: string;
+  followerId: string;
+  followingId: string;
+  follower: IUser;
+  following: IUser;
+}
+
+export interface IBlog {
+  id: string;
+  title: string;
+  content: string;
+  featuredImg: string;
+  published: boolean;
+  user?: IUser;
+  userId?: string;
+  like?: ILike[];
+  categories?: ICategory[];
+  comments?: IComment[];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface ICategory {
+  id: string;
+  name: string;
+  blog?: IBlog[];
+  FollowedUser?: IUser[];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface IComment {
+  id: string;
+  content: string;
+  blogId: string;
+  userId: string;
+  blog?: IBlog;
+  user?: IUser;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface ILike {
+  id: string;
+  blogId: string;
+  userId: string;
+  blog: IBlog;
+  user: IUser;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface IReport {
+  id: string;
+  reporterId: string;
+  reportedId: string;
+  reporter: IUser;
+  reported: IUser;
+  createdAt: Date;
+  updatedAt: Date;
+}
