df179657fc608b0e85c807705e76973f0491711e
Moeenuddin Ahmad <mashruf125@gmail.com>
2024-07-01 20:38:03 +0600

    uplaod images to cloud api intergration

--- lib/uploadImageToCloud.ts
+++ lib/uploadImageToCloud.ts
@@ -1,22 +1,24 @@
 import axios, { AxiosResponse } from "axios";
 
 interface cloudinaryResponse extends AxiosResponse {
-  secure_url: string;
+  data: {
+    secure_url: string;
+  };
 }
 
 const uploadImageToCloud = async (file: File) => {
   try {
     const data = new FormData();
     data.append("file", file);
-    data.append("upload_preset", "tutorial");
+    data.append("upload_preset", "blogApp");
     data.append("cloud_name", "dwzdvokdy");
-
+    data.append("folder", "blogapp");
     const response: cloudinaryResponse = await axios.post(
       "https://api.cloudinary.com/v1_1/dwzdvokdy/image/upload",
       data
     );
-
-    return response?.secure_url;
+    console.log({ response });
+    return response?.data?.secure_url;
   } catch (error) {
     console.error("Error uploading image to Cloudinary:", error);
     return null;
